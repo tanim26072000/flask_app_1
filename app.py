0fe01ccec8460fb09f69dc463596bbf31ed1b00d
tanim26072000 <tanim26072000@gmail.com>
2023-10-09 20:06:53 +0600

    nice fronntend

--- app.py
+++ app.py
@@ -2,10 +2,30 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+jobs= [
+    {
+        'id':1,
+        'title': 'Data analyst',
+        'location': 'dhaka,bd',
+        'salary': 'bdt 20lacs'
+    },
+    {
+        'id':2,
+        'title': 'frontend developer',
+        'location': 'dhaka,bd',
+        'salary': 'bdt 10lacs'
+    },
+    {
+        'id':3,
+        'title': 'backend developer',
+        'location': 'dhaka,bd',
+        'salary': 'bdt 15lacs'
+    },
+]
 
 @app.route('/')
 def hello():
-    return render_template('home.html')
+    return render_template('home.html',jobs= jobs,com_name= 'JIGAOBD')
 
 
 if __name__ == '__main__':
